01b9fb67318d8ae024818501fc063b92847e6de5
Treguboff Vitaliy <tregubovvitaliy1981@gmail.com>
2022-11-30 18:41:47 +0300

    30-11-2022

--- js/service.js
+++ js/service.js
@@ -148,122 +148,128 @@ content_.innerHTML = "";
 /* функция загрузки API */
 async function get_Data(url) {
 
-  if (url == "false") {
-    url = "http://localhost/Test_DB/hs/term/v1/menu";
-  }
+	if (url == "false") {
+		url = "http://localhost/Test_DB/hs/term/v1/menu";
+	}
 
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': "Basic " + btoa((unescape(encodeURIComponent('Integration:Qazxcv135')))),
-      'Content-Type': 'application/json'
-    }
-  });
-  const data = await response.json();
-  return data;
+	const response = await fetch(url, {
+		method: 'GET',
+		headers: {
+			'Authorization': "Basic " + btoa((unescape(encodeURIComponent('Integration:Qazxcv135')))),
+			'Content-Type': 'application/json'
+		}
+	});
+	const data = await response.json();
+	return data;
 }
 
 /* главная функция */
 async function get(container, url) {
 
-  let currentPage = 1;
-  let qty = 9; //на экране 9 кнопок товаров (3х3)
+	let currentPage = 1;
+	let qty = 9; //на экране 9 кнопок товаров (3х3)
 
-  //const serviceData = await get_Data(url);
-  const serviceData = my_test_data;
+	//const serviceData = await get_Data(url);
+	const serviceData = my_test_data;
 
-  function displayList(arrData, qtyPerPage, page) {
-    page--;
-    const content_ = document.querySelector('.content');
-    content_.innerHTML = "";
+	function displayList(arrData, qtyPerPage, page) {
+		page--;
+		const content_ = document.querySelector('.content');
+		content_.innerHTML = "";
 
-    const start = qtyPerPage * page;
-    const end = start + qtyPerPage;
-    const paginatedData = arrData.slice(start, end);
+		const start = qtyPerPage * page;
+		const end = start + qtyPerPage;
+		const paginatedData = arrData.slice(start, end);
 
-    paginatedData.forEach((el) => {
+		paginatedData.forEach((el) => {
 
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-  
-      card.onclick = function () {
-  
-        var isFolder = card.childNodes[1].textContent;//true
-        var codeCard = card.childNodes[2].textContent;//code
-  
-        if (isFolder == "true") {
-          //удалим старую структуру и перестроим ее
-          container.innerHTML = "";
-          
-          const pag = document.querySelector('.pagination');
-          pag.innerHTML = "";
+			const card = document.createElement('div');
+			card.setAttribute('class', 'card');
 
-          get(container, "http://localhost/Test_DB/hs/term/v1/menu?parent_code=" + codeCard);
-        } else {
-          //выбор конечного элемента
-          window.location.href = 'menu.html';
-        }
-  
-      }
-  
-      const h1 = document.createElement('h1');
-      h1.textContent = el.Наименование;
-  
-      const h2 = document.createElement('h2');
-      h2.textContent = el.Группа;
-  
-      const p = document.createElement('h2');
-      p.textContent = el.Код;
-  
-      menu_parent = el.РодительКод;
-  
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(h2);
-      card.appendChild(p);
+			card.onclick = function () {
 
-    })
-  }
+				var isFolder = card.childNodes[1].textContent;//true
+				var codeCard = card.childNodes[2].textContent;//code
 
-  function displayPagination(arrData, qtyPerPage) {
-    const paginationEl = document.querySelector('.pagination');
-    const pagesCount = Math.ceil(arrData.length / qtyPerPage);
-    const ulEl = document.createElement("ul");
-    ulEl.classList.add('pagination__list');
+				if (isFolder == "true") {
 
-    for (let i = 0; i < pagesCount; i++) {
-      const liEl = displayPaginationBtn(i + 1);
-      ulEl.appendChild(liEl)
-    }
-    paginationEl.appendChild(ulEl)
-  }
+					card.style.backgroundColor = 'green'; /* ??? сделать фон папки */
 
-  function displayPaginationBtn(page) {
-    const liEl = document.createElement("li");
-    liEl.classList.add('pagination__item')
-    liEl.innerText = page
+					//удалим старую структуру и перестроим ее
+					container.innerHTML = "";
 
-    if (currentPage == page) liEl.classList.add('pagination__item--active');
+					const pag = document.querySelector('.pagination');
+					pag.innerHTML = "";
 
-    liEl.addEventListener('click', () => {
-      currentPage = page
-      displayList(serviceData, qty, currentPage)
+					get(container, "http://localhost/Test_DB/hs/term/v1/menu?parent_code=" + codeCard);
+				} else {
+					//выбор конечного элемента
+					window.location.href = 'menu.html';
+				}
 
-      let currentItemLi = document.querySelector('li.pagination__item--active');
-      currentItemLi.classList.remove('pagination__item--active');
+			}
 
-      liEl.classList.add('pagination__item--active');
-    })
+			const name_ = document.createElement('div');
+			name_.setAttribute('class', 'name_')
+			name_.textContent = el.Наименование;
 
-    return liEl;
-  }
+			const isFolder_ = document.createElement('div');
+			isFolder_.setAttribute('class', 'isFolder_')
+			isFolder_.textContent = el.Группа;
 
-  /*отражение таблицы товаров*/
-  displayList(serviceData, qty, currentPage);
+			const code_ = document.createElement('div');
+			code_.setAttribute('class', 'code_')
+			code_.textContent = el.Код;
 
-  /* отражение страниц товаров*/
-  displayPagination(serviceData, qty);
-  
+			menu_parent = el.РодительКод;
+
+			container.appendChild(card);
+			card.appendChild(name_);
+			card.appendChild(isFolder_);
+			card.appendChild(code_);
+
+		})
+	}
+
+	function displayPagination(arrData, qtyPerPage) {
+		const paginationEl = document.querySelector('.pagination');
+		const pagesCount = Math.ceil(arrData.length / qtyPerPage);
+		const ulEl = document.createElement("ul");
+		ulEl.classList.add('pagination__list');
+
+		for (let i = 0; i < pagesCount; i++) {
+			const liEl = displayPaginationBtn(i + 1);
+			ulEl.appendChild(liEl)
+		}
+		paginationEl.appendChild(ulEl)
+	}
+
+	function displayPaginationBtn(page) {
+		const liEl = document.createElement("li");
+		liEl.classList.add('pagination__item')
+		liEl.innerText = page
+
+		if (currentPage == page) liEl.classList.add('pagination__item--active');
+
+		liEl.addEventListener('click', () => {
+			currentPage = page
+			displayList(serviceData, qty, currentPage)
+
+			let currentItemLi = document.querySelector('li.pagination__item--active');
+			currentItemLi.classList.remove('pagination__item--active');
+
+			liEl.classList.add('pagination__item--active');
+		})
+
+		return liEl;
+	}
+
+	/*отражение таблицы товаров*/
+	displayList(serviceData, qty, currentPage);
+
+	/* отражение страниц товаров*/
+	displayPagination(serviceData, qty);
+
 }
 
 get(content_, "false"); //первоначальное меню
